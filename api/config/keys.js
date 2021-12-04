ed10aabc8f097ceac8434d959ba743527f53b54f
sfritsch09 <sfritsch09@gmail.com>
2021-12-04 20:20:53 +0100

    Cleared Caches

--- api/config/keys.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-	//We are in production - return the prod set of keys
-	module.exports = require('./prod');
-} else {
-	//We are in development - return the dev set of keys
-	module.exports = require('./dev');
-}
--- api/config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	apiKey: process.env.GOOGLE_API_KEY,
-	calendarID: process.env.GOOGLE_CALENDAR_ID,
-};
